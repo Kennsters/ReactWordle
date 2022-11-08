70d96f9733f3759758226af3e3e3bef9512cb12b
Kenny Yang <kennyyangh@gmail.com>
2022-11-08 00:40:58 -0800

    beginning random word set

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import './App.css';
 import Board from './components/Board'
 import Keyboard from './components/Keyboard'
-import { createContext, useState } from 'react'
-import { boardDefault } from "./Words"
+import { createContext, useEffect, useState } from 'react'
+import { boardDefault, generateWordSet } from "./Words"
 
 export const AppContext = createContext();
 
@@ -10,6 +10,14 @@ function App() {
 
   const [board, setBoard] = useState(boardDefault);
   const [currAttempt, setCurrAttempt] = useState({attempt: 0, letterPos: 0})
+
+  const correctWord = "RIGHT"
+
+  useEffect(() => {
+    generateWordSet().then((words) => {
+      console.log(words)
+    })
+  }, [])
 
   const onSelectLetter = (keyVal) => {
     if (currAttempt.letterPos > 4) return;
@@ -37,7 +45,7 @@ function App() {
       <nav>
         <h1>Wordle</h1>
       </nav>
-      <AppContext.Provider value={{ board, setBoard, currAttempt, setCurrAttempt, onSelectLetter, onDelete, onEnter }}>
+      <AppContext.Provider value={{ board, setBoard, currAttempt, setCurrAttempt, onSelectLetter, onDelete, onEnter, correctWord }}>
         <div className="game">
           <Board />
           <Keyboard />
--- src/components/Board.js
+++ src/components/Board.js
@@ -21,28 +21,28 @@ function Board() {
       </div>
       <div className="row">
         <Letter letterPos={0} attemptVal={2} />
-        <Letter letterPos={2} attemptVal={2} />
+        <Letter letterPos={1} attemptVal={2} />
         <Letter letterPos={2} attemptVal={2} />
         <Letter letterPos={3} attemptVal={2} />
         <Letter letterPos={4} attemptVal={2} />
       </div>
       <div className="row">
         <Letter letterPos={0} attemptVal={3} />
-        <Letter letterPos={3} attemptVal={3} />
+        <Letter letterPos={1} attemptVal={3} />
         <Letter letterPos={2} attemptVal={3} />
         <Letter letterPos={3} attemptVal={3} />
         <Letter letterPos={4} attemptVal={3} />
       </div>
       <div className="row">
         <Letter letterPos={0} attemptVal={4} />
-        <Letter letterPos={4} attemptVal={4} />
+        <Letter letterPos={1} attemptVal={4} />
         <Letter letterPos={2} attemptVal={4} />
         <Letter letterPos={3} attemptVal={4} />
         <Letter letterPos={4} attemptVal={4} />
       </div>
       <div className="row">
         <Letter letterPos={0} attemptVal={5} />
-        <Letter letterPos={5} attemptVal={5} />
+        <Letter letterPos={1} attemptVal={5} />
         <Letter letterPos={2} attemptVal={5} />
         <Letter letterPos={3} attemptVal={5} />
         <Letter letterPos={4} attemptVal={5} />
